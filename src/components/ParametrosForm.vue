1c5470ff2354ac13e1c7f681d28367a64995ba40
Juan Carlos González <juancg78@hotmail.com>
2026-03-08 20:11:58 -0500

    Mejoras visuales, eliminar archivos del proyecto base

--- src/components/ParametrosForm.vue
+++ src/components/ParametrosForm.vue
@@ -1,6 +1,5 @@
 <template>
   <form @submit.prevent="emitirCalculo" class="form-container">
-    <h3>Configuración del Usuario</h3>
     <label>Sexo:
       <select v-model="formParams.sexo">
         <option value="M">Masculino</option>
@@ -18,14 +17,16 @@
     <label>Tiempo de Simulación (minutos):
       <input type="number" v-model.number="formParams.tiempoTotalMin" min="30" max="480" />
     </label>
-
-    <h3>Configuración de la Bebida</h3>
-    <label>Tipo de Bebida (% Alcohol):
+    <label>Temp. Ambiente (°C):
+      <input type="number" v-model.number="formParams.tempFinal" step="0.1" />
+    </label>
+<hr />
+    <label>Bebida (% Alcohol):
       <select v-model.number="formParams.porcentajeAlcohol">
         <option v-for="opc in tiposBebida" :key="opc.alcohol" :value="opc.alcohol">{{ opc.etiqueta }}</option>
       </select>
     </label>
-    <label>Tamaño de la Bebida (cc):
+    <label>Volumen Bebida (cc):
         <select v-model.number="formParams.volumenBebida">
             <option v-for="opc in tamanosBebida" :key="opc.volumen" :value="opc.volumen">{{ opc.etiqueta }}</option>
         </select>
@@ -33,16 +34,13 @@
     <label>Volumen por Trago (cc):
         <input type="number" v-model.number="formParams.volumenTrago" min="10" max="100" />
     </label>
-    <label>Tiempo míninimo entre tragos (minutos):
+    <label>Tiempo mínimo entre tragos (minutos):
         <input type="number" v-model.number="formParams.minTiempoEntreTragos" min="1" max="60" />
     </label>
-    <label>Temp. Inicial (°C):
+    <label>Temp. inicial bebida (°C):
       <input type="number" v-model.number="formParams.tempInicial" step="0.1" />
     </label>
-    <label>Temp. Ambiente (°C):
-      <input type="number" v-model.number="formParams.tempFinal" step="0.1" />
-    </label>
-    <label>Calentamiento (°C/min):
+    <label>Calentamiento bebida (°C/min):
       <input type="number" v-model.number="formParams.deltaTemp" step="0.01" />
     </label>
 
@@ -77,7 +75,7 @@ const emitirCalculo = () => {
 </script>
 
 <style scoped>
-.form-container { display: grid; grid-template-columns: 1fr 1fr; gap: 15px; background: #f9f9f9; padding: 20px; border-radius: 8px; }
+.form-container { display: grid; grid-template-columns: repeat(auto-fit, minmax(180px, 1fr)); gap: 15px; background: #f9f9f9; padding: 20px; border-radius: 8px; }
 h3 { grid-column: 1 / -1; margin-bottom: 5px; color: #333; }
 label { display: flex; flex-direction: column; font-size: 14px; font-weight: bold; }
 input, select { padding: 8px; margin-top: 5px; border: 1px solid #ccc; border-radius: 4px; }
