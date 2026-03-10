ad75b3361b1626f3a651b15a4906665f4b852f60
Juan Carlos González <juancg78@hotmail.com>
2026-03-10 13:01:03 -0500

    ajustar base

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/OtraRondaApp/',
+  base: '/otraronda/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
